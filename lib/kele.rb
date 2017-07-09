0031c4134f8acfbed20f3443582ebe5b6b1c87a1
Saranya Ganapathy <saranyaganapathy95@gmail.com>
2017-07-09 14:15:43 -0400

    Checkpoint 6 Messaging complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -14,19 +14,28 @@ class Kele
 
   def get_me
     response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
-    @user_data = JSON.parse(response.body)
-    @user_data.keys.each do |key|
+    user_data = JSON.parse(response.body)
+    user_data.keys.each do |key|
       self.class.send(:define_method, key.to_sym) do
-        @user_data[key]
+        user_data[key]
       end
     end
   end
 
   def get_mentor_availability(mentor_id)
     response = self.class.get(base_api_endpoint("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
-    @mentor_availability = JSON.parse(response.body)
+    mentor_availability = JSON.parse(response.body)
   end
 
+  def get_messages(page)
+    response = self.class.get(base_api_endpoint("message_threads?page=#{page}"), headers: { "authorization" => @auth_token })
+    get_messages = JSON.parse(response.body)
+  end
+
+  def create_messages(recipient_id, subject, message)
+    response = self.class.post(base_api_endpoint("messages"), body: { "user_id": id, "recipient_id": recipient_id, "subject": subject, "stripped-text": message }, headers: { "authorization" => @auth_token })
+    puts response
+  end
   private
 
   def base_api_endpoint(end_point)
